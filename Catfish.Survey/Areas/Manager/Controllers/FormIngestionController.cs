1616f30b73774bd081551e91924820df2f392b27
Mark McKellar <mpm1@ualberta.ca>
2018-10-17 11:49:48 -0600

    Initial Commit

---
+++ Catfish.Survey/Areas/Manager/Controllers/FormIngestionController.cs
@@ -0,0 +1,18 @@
+﻿using Catfish.Areas.Manager.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Catfish.Survey.Areas.Manager.Controllers
+{
+    public class FormIngestionController : CatfishController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Catfish.Survey/SurveyPlugin.cs
@@ -0,0 +1,45 @@
+﻿using Catfish.Core.Plugins;
+using Piranha.WebPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Catfish.Survey
+{
+    public class SurveyPlugin : Plugin
+    {
+        public override void Initialize()
+        {
+            InitializeManagerViews();
+            InitializeComponents();
+        }
+
+        private void InitializeComponents()
+        {
+            
+        }
+
+        private void InitializeManagerViews()
+        {
+            var menubar = Manager.Menu.Where(m => m.InternalId == "Settings").FirstOrDefault();
+
+            menubar.Items.Insert(1, new Manager.MenuItem()
+            {
+                Name = "Ingest Forms",
+                Action = "Index",
+                Controller = "FormIngestion",
+                Permission = "ADMIN_CONTENT",
+            });
+
+            AreaRegistration.RegisterAllAreas();
+        }
+
+        public virtual void RegisterArea(AreaRegistrationContext context)
+        {
+            context.Namespaces.Add("Catfish.Survey");
+        }
+    }
+}
