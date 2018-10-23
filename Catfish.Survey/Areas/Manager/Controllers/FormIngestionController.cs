cfdff81a2e80c1a4cd25546d1bf5157593c39775
Mark McKellar <mpm1@ualberta.ca>
2018-10-23 10:27:35 -0600

    Added latest controller.

--- Catfish.Survey/Areas/Manager/Controllers/FormIngestionController.cs
+++ Catfish.Survey/Areas/Manager/Controllers/FormIngestionController.cs
@@ -1,15 +1,14 @@
-﻿using Catfish.Areas.Manager.Controllers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Catfish.Survey.Areas.Manager.Controllers
 {
-    public class FormIngestionController : CatfishController
+    public class FormIngestionController : Controller
     {
+        // GET: Manager/FormIngestion
         public ActionResult Index()
         {
             return View();
